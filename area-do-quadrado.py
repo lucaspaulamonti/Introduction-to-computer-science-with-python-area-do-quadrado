76d6cbd24d3c5dfbb2b3a6fa5b646c3de778ef0d
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-22 15:30:02 -0300

    Create area-do-quadrado.py
    main content

---
+++ area-do-quadrado.py
@@ -0,0 +1,5 @@
+# Faça um programa em Python que receba (entrada de dados) o valor correspondente ao lado de um quadrado, calcule e imprima (saída de dados) seu perímetro e sua área.
+lado=int(input("Digite o valor correspondente ao lado de um quadrado: "))
+print("perímetro:",lado*4,"- área:",lado*lado)
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso!
